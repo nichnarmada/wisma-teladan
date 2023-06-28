7edfd4ae6341d3e70c78adcfac01d86785711da8
Nicholas Narmada <nichnarmada@gmail.com>
2023-06-28 16:28:35 +0800

    feat: added header and footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import Link from "next/link"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
-import { ModeToggle } from "@/components/mode-toggle"
+// import { ModeToggle } from "@/components/mode-toggle"
+import { Header } from "@/components/layout/header"
+import { Footer } from "@/components/layout/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,28 +17,37 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
+export type tabDetail = { href: string; name: string }
+
+const tabList: tabDetail[] = [
+  { href: "/", name: "Home" },
+  { href: "/about", name: "Tentang Kami" },
+  { href: "/service", name: "Pelayanan" },
+  { href: "/experience", name: "Pengalaman" },
+  { href: "/gallery", name: "Galeri" },
+  { href: "/contact", name: "Kontak" },
+]
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
+        className={`min-h-screen bg-white text-slate-900 antialiased dark:bg-slate-950 dark:text-slate-50 ${inter.className}`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="max-w-2xl mx-auto py-10 px-4">
-            <header>
-              <div className="flex items-center justify-between">
+          <header>
+            <Header tabs={tabList} />
+            {/* <div className="flex items-center justify-between">
                 <ModeToggle />
-                <nav className="ml-auto text-sm font-medium space-x-6">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
-                </nav>
-              </div>
-            </header>
+              </div> */}
+          </header>
+          <div className="mx-auto max-w-2xl px-4 py-10">
             <main>{children}</main>
           </div>
           <Analytics />
         </ThemeProvider>
       </body>
+      <Footer />
     </html>
   )
 }
