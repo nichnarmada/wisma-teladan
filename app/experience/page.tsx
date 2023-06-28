e1c639a3ee73d0681ba492294f6c38bceb490cc3
Nicholas Narmada <nichnarmada@gmail.com>
2023-06-28 16:27:42 +0800

    feat: update experience and service page content

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,17 +1,5 @@
-import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 
 export default function Experience() {
-  return (
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
-    </div>
-  )
+  return <div className="prose dark:prose-invert">Experience</div>
 }
