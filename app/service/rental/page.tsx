4671a209ee30d28fb0d4f68216db73fda11d2e51
Nicholas Narmada <nichnarmada@gmail.com>
2023-07-01 17:17:18 +0700

    feat: change content file paths

---
+++ app/service/rental/page.tsx
@@ -0,0 +1,234 @@
+const products = [
+  {
+    id: 1,
+    name: "Ingersoll Rand SD 180 D",
+    href: "",
+    type: "Compactor Pad Foot",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 2,
+    name: "Caterpillar CS 76 XT",
+    href: "",
+    type: "Compactor Pad Foot",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 3,
+    name: "Bomag BW 219 D",
+    href: "",
+    type: "Compactor Pad Foot",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 4,
+    name: "Sakai SV 512 TF",
+    href: "",
+    type: "Compactor Pad Foot",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  {
+    id: 5,
+    name: "Ingersoll Rand SD 180 D",
+    href: "",
+    type: "Compactor Smooth Drum",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 6,
+    name: "Caterpillar 120 H",
+    href: "",
+    type: "Motor Grader",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 7,
+    name: "Caterpillar PM 620",
+    href: "",
+    type: "Cold Milling",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 8,
+    name: "Caterpillar 120 G",
+    href: "",
+    type: "Motor Grader",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  {
+    id: 9,
+    name: "Ingersoll Rand SD 180 D",
+    href: "",
+    type: "Compactor Pad Foot",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 10,
+    name: "Streumaster 15 T",
+    href: "",
+    type: "Semen Spreader",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 11,
+    name: "Caterpillar 320 C",
+    href: "",
+    type: "Excavator",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 12,
+    name: "Caterpillar 320 B",
+    href: "",
+    type: "Excavator",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  {
+    id: 13,
+    name: "Caterpillar 320 CL",
+    href: "",
+    type: "Excavator",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  {
+    id: 14,
+    name: "Caterpillar CS 683",
+    href: "",
+    type: "Compactor Smooth Drum",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 15,
+    name: "Sakai SV512D",
+    href: "",
+    type: "Compactor Smooth Drum",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 16,
+    name: "Bomag BW 219 D",
+    href: "",
+    type: "Compactor Smooth Drum",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 17,
+    name: "Caterpillar 350 B",
+    href: "",
+    type: "Reclaimer Mixer",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 18,
+    name: "Tronton Hino",
+    href: "",
+    type: "Dump Truck",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 4,
+    name: "Water Tank 1500L",
+    href: "",
+    type: "Water Tank",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  // More products...
+]
+
+export default function Rental() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="pb-24 text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+            Sewa Alat Berat
+          </h1>
+          {/* <p className="mx-auto mt-4 max-w-3xl text-base text-gray-500">
+            Thoughtfully designed objects for the workspace, home, and travel.
+          </p> */}
+        </div>
+
+        <h2 className="sr-only">Products</h2>
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <a
+              key={product.id}
+              href={product.href}
+              className="group pointer-events-none"
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  // group-hover:opacity-75
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+              <p className="mt-4 text-sm text-gray-700">{product.type}</p>
+              <h3 className="mt-1 text-lg font-medium text-gray-900">
+                {product.name}
+              </h3>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
